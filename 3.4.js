471c47706261273e4ea1b40ff9d04db859308bf3
Daichi Nasu <gtvsa22@gmail.com>
2012-07-17 16:42:22 +0900

    code is efficient
    code changed

--- 3.4.js
+++ 3.4.js
@@ -6,47 +6,37 @@ var DIR_DOWN = 3;
 
 
 enchant();
+
+Square = Class.create(Sprite, {
+ initialize:function(x, y){
+  enchant.Sprite.call(this, 125, 125);
+  //this.image = game.assets[]
+  this.x = x;
+  this.y = y;
+  }
+  });
+  
 window.onload = function(){
-	var game = new Game(1026, 1026);
-	game.fps = 120;
-	
-	/*var rand = function(num){ return (Math.random() * num ) | 0;}
-	
-	var Grid = enchant.Class.create(Group, {
-	initialize : function() { 
-		Group.call(this);
-		for(var i = 0; i < 4; i++){
-			var leftLine = new Line((i % 4) * 80, 0);
-			this.addChild(leftLine);
-			var rightLine = new Line((i % 4) * 80 + 79, 0);
-			this.addChild(rightLine);
-			var topLine = new Line(160, (i % 4) * 80 -160);
-			topLine.rotate(90);
-			this.addChild(topLine);
-			var bottomLine = new Line (160, (i %4) * 80 - 81);
-			bottomLine.rotate(90);
-			this.addChild(bottomLine);
-			}
-		}
-	});*/
+	var game = new Game(1000, 1000);
+	game.fps = 16;
+
 	game.preload('http://blog-imgs-54.fc2.com/g/t/v/gtvsa/IMG.jpg' ,
 				'http://blog-imgs-54.fc2.com/g/t/v/gtvsa/pazzle.jpg');
 				
-							
-		game.onload = function() {
+	game.onload = function() {
 	
 	
 	
 	//背景の生成
-	var bg = new Sprite(1023, 1026);
-	var maptip = game.assets['http://blog-imgs-54.fc2.com/g/t/v/gtvsa/pazzle.jpg'];
-	var image = new Surface(960, 960);
+	var bg = new Sprite(1000, 1000);
+	bg.image = game.assets['http://blog-imgs-54.fc2.com/g/t/v/gtvsa/pazzle.jpg'];
+	/*var image = new Surface(1026, 1026);
 	for (var j = 0; j < 960; j += 32) {
 	for (var i = 0; i < 960; i += 32){
-		image.draw(maptip, 0, 0, 32, 32, i , j, 32, 32);
+		image.draw(maptip);
 		}
 	}
-	bg.image = image;
+	bg.image = image;*/
 	game.rootScene.addChild(bg);
 
 		//サーフェイスの色指定メソッドの追加
@@ -68,32 +58,31 @@ window.onload = function(){
 		var surface = new Surface(1000, 1000);
 		
 		surface.setColor(color);
-		if(type ==0) surface.drawRect(0, 0,900, 900);
-		
+		if(type ==0) surface.drawRect(0, 0, 940, 940);
+	
 		//スプライトの作成
-		
-		var sprite = new Sprite(1000, 1000);
-		sprite.image=surface;
-		sprite.x = x;
-		sprite.y = y;
-		game.rootScene.addChild(sprite);
+		var square = new Square (1000, 1000);
+		square.image=surface;
+		square.x = x;
+		square.y = y;
+		game.rootScene.addChild(square);
 	};
-	game.addSurface(0, "black", 50, 50 );
+	game.addSurface(0, "black", 50, 50 ); 
 	
 	//キャラクターの生成
-	var mary = new Sprite(380, 370);
+	var mary = new Sprite(250, 250);
 	mary.image =game.assets['http://blog-imgs-54.fc2.com/g/t/v/gtvsa/IMG.jpg'];
-	mary.x 		= 160 - 16;
-	mary.y 		= 160 - 16;
+	mary.x 		= 500 - 125;
+	mary.y 		= 500 - 125;
 	mary.frame  = 7 ;
 	mary.toX 	= mary.x;
 	mary.toY 	= mary.y;
 	mary.dir 		= DIR_DOWN;
-	mary.anim	= [
+	/*mary.anim	= [
 			15, 16, 17, 16, //左
 			24, 25, 26, 24, //右
 			33, 34, 35, 34, //上
-			6, 7,  8,  7]; //下
+			6, 7,  8,  7]; //下*/
 	game.rootScene.addChild(mary);
 	mary.tick = 0 ;
 	mary.addEventListener(Event.ENTER_FRAME, function() {
@@ -136,28 +125,22 @@ window.onload = function(){
 		//フレームの指定
 		mary.tick++;
 		if ( mary.x == mary.toX && mary.y == mary.toY) mary.tick = 1;
-		mary.frame = mary.anim[mary.dir * 4 + (mary.tick % 4)];
+		mary.frame = mary.dir * 4 + (mary.tick % 4);
 	});
 
 
 	//タッチ開始時に呼ばれる
-	bg.addEventListener(Event.TOUCH_START, function(e) {
-			mary.toX = e.x - 190;
-			mary.toY = e.y - 190;
+	game.addEventListener(Event.TOUCH_START, function(e) {
+			mary.toX = e.x - 125;
+			mary.toY = e.y - 125;
 		});
 		
 	//タッチ移動時に呼ばれる
-	bg.addEventListener(Event.TOUCH_MOVE, function(e) {
-			mary.toX = e.x - 190 ;
-			mary.toY = e.x - 190 ;
+	game.addEventListener(Event.TOUCH_MOVE, function(e) {
+			mary.toX = e.x - 125 ;
+			mary.toY = e.x - 125 ;
 		});
 	};
-	//var grid = new Grid();
-	//game.rootScene.addChild(grid);
-		
-			
-
-	
 		
 	game.start();
 };
