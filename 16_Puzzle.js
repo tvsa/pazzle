4e86eefbc68114452cb302df5010124cfb52b48e
Daichi Nasu <gtvsa22@gmail.com>
2012-08-01 14:27:08 +0900

    Forked

--- 16_Puzzle.js
+++ 16_Puzzle.js
@@ -1,40 +1,24 @@
-﻿var IMG = "iMG.jpg";
+﻿//定数化
+var IMG = "iMG.jpg";
+var DIV = 4; //divide
+var GAME_WIDTH = 320;
+var GAME_HEIGHT = 320;
+var TILE_WIDTH = GAME_WIDTH / DIV; //タイル一枚の横幅
+var TILE_HEIGHT = GAME_HEIGHT / DIV;//タイル一枚の縦幅
 enchant();
 
-var test = new Array(4);
-for (var i = 0; i < 4; i++) {
-    test[i] = new Array(4);
+var tile = new Array(DIV);
+for (var i = 0; i < DIV; i++) {
+    tile[i] = new Array(DIV);
 }
-
+//スプライトの定数化
 var Girl = enchant.Class.create(enchant.Sprite, {
-    initialize: function (root, _X, _Y, _img, _fc) {
-        enchant.Sprite.call(this, 80, 80);
-        this.image = _img; this.id = _fc;
-        test[_X][_Y] = _fc;
-        this.x = 80 * _X; this.y = 80 * _Y;
+    initialize: function (_X, _Y, _img, _fc) {
+        enchant.Sprite.call(this,TILE_WIDTH , TILE_HEIGHT);
+        this.image = _img;
+        this.id = _fc;
+        this.x = TILE_WIDTH * _X; this.y = TILE_HEIGHT * _Y;
         this.frame = _fc;
-        this.addEventListener(Event.TOUCH_START, function () {
-            var i = this.x / 80;
-            var j = this.y / 80;
-            if (test[i][j + 1] == -1) {
-                test[i][j + 1] = this.id;
-                this.y = this.y + 80;
-                test[i][j] = -1;
-            } else if (test[i][j - 1] == -1) {
-                test[i][j - 1] = this.id;
-                this.y = this.y - 80;
-                test[i][j] = -1;
-            } else if (test[i + 1][j] == -1) {
-                test[i + 1][j] = this.id;
-                this.x = this.x + 80;
-                test[i][j] = -1;
-            } else if (test[i - 1][j] == -1) {
-                test[i - 1][j] = this.id;
-                this.x = this.x - 80;
-                test[i][j] = -1;
-            }
-        });
-        root.addChild(this);
     }
 });
 
@@ -42,20 +26,42 @@ window.onload = function () {
     var game = new Game(320, 320);
     game.fps = 16;
     game.preload(IMG);
-
-    var fc = 0;  
-    game.onload = function(){  
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < 4; j++) {
-                if (i == 3 && j == 3) {
-                    test[i][j] = -1;
+    //Sprite一枚一枚に振ったID
+    var fc =[1,0,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    game.onload = function () {
+        //ここから描画
+        for (var y = 0; y < DIV; y++) {
+            for (var x = 0; x < DIV; x++) {
+                if (y == DIV-1 && x == DIV -1) {
+                    tile[y][x] = -1; //空白
                 } else {
-                    new Girl(game.rootScene, j, i, game.assets[IMG], fc);
-                    fc++;
+                    tile[y][x] = fc[y * DIV + x];
+                    var girl = new Girl(x, y, game.assets[IMG], tile[y][x]);
+                    game.rootScene.addChild(girl);
+                    girl.addEventListener(Event.TOUCH_START, function () {
+                        var i = this.y / TILE_HEIGHT;
+                        var j = this.x / TILE_WIDTH;
+                        if (j < DIV - 1 && tile[i][j + 1] == -1) {
+                            tile[i][j + 1] = this.id;
+                            this.x +=  TILE_WIDTH;
+                            tile[i][j] = -1;
+                        } else if (0 < j  && tile[i][j - 1] == -1) {
+                            tile[i][j - 1] = this.id;
+                            this.x -= TILE_WIDTH;
+                            tile[i][j] = -1;
+                        } else if (i < DIV - 1 && tile[i + 1][j] == -1) {
+                            tile[i + 1][j] = this.id;
+                            this.y += TILE_HEIGHT;
+                            tile[i][j] = -1;
+                        } else if (0 < i && tile[i - 1][j] == -1) {
+                            tile[i - 1][j] = this.id;
+                            this.y -= TILE_HEIGHT;
+                            tile[i][j] = -1;
+                        }
+                    });                    
                 }
             }
         }
-
     };
     game.start();
 };
